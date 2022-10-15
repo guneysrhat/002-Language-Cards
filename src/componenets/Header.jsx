4fc8aa8ba479913c6d1ff540b3f3e97e193f995c
guneysrhat <guneysrhat@gmail.com>
2022-10-15 11:57:13 +0300

    header

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import reactLogo from "../assets/react.svg";
 
 const Header = () => {
-  return <Container>Header</Container>;
+  return (
+    <Container >
+      <Image fluid src={reactLogo}></Image>
+    </Container>
+  );
 };
 
 export default Header;
