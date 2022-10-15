ec6685631a4288737f54376bbc0d7f0acaa44c65
guneysrhat <guneysrhat@gmail.com>
2022-10-15 13:21:41 +0300

    use state

--- src/componenets/Language.jsx
+++ src/componenets/Language.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 
 const Language = ({ name, img, options }) => {
-  //console.log(lang);
+  const [showDesc, setShowDesc] = useState(false);
+  // console.log(name, img, options);
+  console.log(showDesc);
   return (
     <Container>
-      <Container className="rounded-3 p-4" style={{ background: "peachpuff" }}>
+      <Container
+        className="rounded-3 p-4"
+        style={{ background: "peachpuff" }}
+        onClick={() => setShowDesc(!showDesc)}
+      >
         <Image src={img} width="70%"></Image>
         <h3 className="display-6">{name}</h3>
       </Container>
