72651f2de1c06782a5348757b4d4a34f140108c2
guneysrhat <guneysrhat@gmail.com>
2022-10-15 13:58:12 +0300

    commit

--- src/componenets/Language.jsx
+++ src/componenets/Language.jsx
@@ -3,19 +3,29 @@ import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 
 const Language = ({ name, img, options }) => {
-  const [showDesc, setShowDesc] = useState(false);
+  const [showDesc, setShowDesc] = useState(true);
   // console.log(name, img, options);
   console.log(showDesc);
   return (
-    <Container>
-      <Container
-        className="rounded-3 p-4"
-        style={{ background: "peachpuff" }}
-        onClick={() => setShowDesc(!showDesc)}
-      >
-        <Image src={img} width="70%"></Image>
-        <h3 className="display-6">{name}</h3>
-      </Container>
+    <Container
+      className="rounded-3 p-4 h-100 land-card"
+      style={{ background: "peachpuff" }}
+      onClick={() => setShowDesc(!showDesc)}
+      type="button"
+    >
+      {showDesc && (
+        <Container>
+          <Image className="lang-logo" src={img} width="70%"></Image>
+          <h3 className="display-6 ">{name}</h3>
+        </Container>
+      )}
+      {!showDesc && (
+        <ul className="h-100">
+          {options.map((item) => {
+            return <li className="h5 text-start">{item}</li>;
+          })}
+        </ul>
+      )}
     </Container>
   );
 };
